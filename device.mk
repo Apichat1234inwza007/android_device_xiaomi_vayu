319369e6d333dc21f3545f5618e01d53189e5da2
Apichat1234inwza007 <discom610@gmail.com>
2025-04-29 06:35:55 +0000

    vayu: fix commit "overlay: Set default hotspot ssid"

--- device.mk
+++ device.mk
@@ -449,7 +449,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-<<<<<<< HEAD
+# Overlay-RRO
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
@@ -474,11 +477,6 @@ PRODUCT_PACKAGES += \
 
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
-=======
-# Overlay-RRO
-PRODUCT_PACKAGES += \
-    TargetWifiOverlay
->>>>>>> e937820 (vayu: overlay: Set default hotspot ssid)
 
 # QDCM
 PRODUCT_COPY_FILES += \
